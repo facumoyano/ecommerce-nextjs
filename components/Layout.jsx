5bbd1703dd80d6a4e16917421b78f5c7721bbe65
facumoyano <89871383+facumoyano@users.noreply.github.com>
2022-07-15 21:25:14 -0300

    primer commit

---
+++ components/Layout.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+    return (
+        <div className="layout">
+            <Head>
+                <title>Store NextJS</title>
+            </Head>
+            <Navbar />
+            <main className="main-container">{children}</main>
+            <footer>
+                <Footer />
+            </footer>
+        </div>
+    );
+};
+
+export default Layout;
